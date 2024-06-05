ba12f4a4b28b26d689be2fc7955fd634bf7cb074
rallmac2 <root@tobijahdevice>
2024-06-05 12:25:21 +0100

    task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Inserts a binary tree
+ * @parent: The root Node
+ * @value: The value of node
+ * Return: 0 always (success)
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode = binary_tree_node(parent, value);
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	if (parent->left != NULL)
+	{
+		newNode->left = parent->left;
+		parent->left->parent = newNode;
+	}
+	parent->left = newNode;
+	return(newNode);
+}
