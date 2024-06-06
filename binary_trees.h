ea699c2dae8d316baf89107c766840dc67333731
rallmac2 <root@tobijahdevice>
2024-06-06 22:28:18 +0100

    task 133 redo

--- binary_trees.h
+++ binary_trees.h
@@ -38,6 +38,7 @@ typedef struct link_s
 } link_t;
 
 /* Prototypes */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
